fb3e705065586df560b4d5322c39d9869c0f5290
bbvenega <bbvenega@asu.edu>
2024-05-10 13:00:05 -0700

    Weather Functionality (BACKEND)
    + Added ability to call NWS weather API based on user's address.
    
    + Returns nearest office to address, then gives user options between
    
    "Forecast" ~ forecast for 12h periods over the next seven days
    
    "forecastHourly" ~ forecast for hourly periods over the next seven days
    
    "forecastGridData" ~ raw forecast data over the next seven days

--- weatherApp.py
+++ weatherApp.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def get_location_options(address, api_key):
 
@@ -20,6 +21,7 @@ def get_location_options(address, api_key):
     
 
 if __name__ == "__main__":
+    
     address = input("Please enter your address: ")
 
     api_key = 'AIzaSyCzNKaGvIkGHx1LwUE32j6ua89fLIkgKPc'
@@ -29,3 +31,48 @@ if __name__ == "__main__":
 
 if lat is not None and lng is not None: 
     print(f'Latitiude: {lat}, Longitutde: {lng}')
+
+    officeUrl = f'https://api.weather.gov/points/{lat},{lng}'
+    
+    officeResponse = requests.get(officeUrl)
+
+    if officeResponse.status_code == 200:
+
+        officeForecast = officeResponse.json()
+        json_string = json.dumps(officeForecast, indent=4)
+        print(json_string)
+
+
+        
+
+        officeOptions = [
+            officeForecast['properties']['forecast'],
+            officeForecast['properties']['forecastHourly'],
+            officeForecast['properties']['forecastGridData']
+        ]
+
+        counter = 1
+        for element in officeOptions:
+         print(counter, element)
+         counter += 1
+
+        user_input = input("Which would you like to see? : ")
+        selectedWeatherService = int(user_input)
+        weatherUrl = officeOptions[selectedWeatherService - 1]
+
+        weatherResponse = requests.get(weatherUrl)
+
+        if(weatherResponse.status_code == 200):
+
+            weatherForecast = weatherResponse.json()
+            json_weather = json.dumps(weatherForecast, indent = 4)
+            print(json_weather)
+        
+        else: 
+            print(f"Error: Not a valid choice")
+
+    else: 
+        print(f"Error: Unable to fetch office forecase. Status code : {officeResponse.status_code}")
+    
+    
+    
