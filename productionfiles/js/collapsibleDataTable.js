13b410dfcd9c9931c6e862a206262ef36774b4fc
bbvenega <bbvenega@asu.edu>
2024-05-14 19:20:36 -0700

    Front End Tweaks
    + Continued work on Positioning of text in Daily weather forecast

--- productionfiles/js/collapsibleDataTable.js
+++ productionfiles/js/collapsibleDataTable.js
@@ -1,15 +1,6 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for(i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-
-        var content = this.nextElementSibling;
-        if(content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-        });
-    }
+var $headers = $('.header').click(function () {
+    $(this).find('span').text(function (_, value) {
+        return value == '-' ? '+' : '-'
+    });
+    $(this).nextUntil('tr.header').slideToggle(100, function () {});
+});
